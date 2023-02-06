701353d4ed2d33f8403b517ed8213ca2d21ac33d
Joy Gu <joy.alice.gu@gmail.com>
2023-02-05 20:50:19 -0800

    2022 day 17 part 2

--- 2022/17/solution.py
+++ 2022/17/solution.py
@@ -79,39 +79,90 @@ class Rock:
         return
 
 class Chamber:
-    def __init__(self, jets):
+    def __init__(self, jets, target):
         self.chamber = [CHAMBER_FLOOR] # only contains non-empty rows
         self.rock_number = 0
         self.rock_index = 0
         self.jet_index = 0
         self.jets = jets
+        self.target = target # total rocks to drop
+        # for cycle detection
+        self.offset = 0 # offset to add to height
+        self.placements = [] # placement history
+        self.heights = [] # height history
+        self.skipped = False
+        self.cycle_detected = False
+        self.cycle_length = None # rocks dropped during cycle
+        self.cycle_height = None # height gained during cycle
 
     def drop_rock(self):
         rock = Rock(self.rock_index, len(self.chamber) + BOTTOM_PADDING)
+        x, y = 0, 0
         while not rock.stopped:
-            # move horizontally (if possible)
             direction = self.jets[self.jet_index]
-            rock.move_horizontal(direction, self.chamber)
             self.jet_index = (self.jet_index + 1) % len(self.jets)
+            # move horizontally (if possible)
+            success = rock.move_horizontal(direction, self.chamber)
+            if success:
+                x += 1 if direction == '<' else -1
             # move down (if possible)
-            rock.move_down(self.chamber)
+            success = rock.move_down(self.chamber)
+            if success:
+                y += 1
+        self.placements.append((x, y, self.rock_index, self.jet_index))
+        self.heights.append(self.height())
         self.rock_number += 1
         self.rock_index = self.rock_number % len(ROCKS)
+        self.detect_cycle()
         return
 
     def height(self):
-        return len(self.chamber) - 1
+        return len(self.chamber) - 1 + self.offset
 
     def print(self):
         for line in reversed(self.chamber):
             print(bin(line))
 
-def simulate(rock_count, jets):
-    chamber = Chamber(jets)
-    for _ in range(rock_count):
-        chamber.drop_rock()
-    return chamber.height()
+    def detect_cycle(self): # called after each rock is dropped
+        hare = len(self.placements) - 1
+        tortoise = hare // 2
+        if hare % 2 != 0 or hare == tortoise:
+            return
+        if self.placements[tortoise] != self.placements[hare]:
+            return
+        # assume the tortoise and the hare are both in the cycle
+        # assume the distance between them is a multiple of the cycle length
+        self.cycle_detected = True
+        self.cycle_length = hare - tortoise
+        self.cycle_height = self.heights[hare] - self.heights[tortoise]
+        # find mu (just for fun)
+        mu = 0
+        tortoise, hare = 0, tortoise
+        while self.placements[tortoise] != self.placements[hare]:
+            tortoise += 1
+            hare += 1
+            mu += 1
+        print("beginning of the first cycle:", mu)
+        # find lam (just for fun)
+        lam = 1
+        tortoise, hare = mu, mu + 1
+        while self.placements[tortoise] != self.placements[hare]:
+            hare += 1
+            lam += 1
+        print("length of the shortest cycle:", lam)
+        return
+
+    def simulate(self):
+        while self.rock_number < self.target:
+            if self.cycle_detected and not self.skipped: # skip remaining cycles
+                remaining_rocks = self.target - self.rock_number
+                remaining_cycles = remaining_rocks // self.cycle_length
+                self.rock_number += remaining_cycles * self.cycle_length
+                self.offset = remaining_cycles * self.cycle_height
+                self.skipped = True
+            self.drop_rock()
+        return self.height()
 
 JETS = parse_jets("input.txt")
-print("Part 1:", simulate(2022, JETS))
-# print("Part 2:", simulate(1000000000000, JETS))
+print("Part 1:", Chamber(jets=JETS, target=2022).simulate())
+print("Part 2:", Chamber(jets=JETS, target=1000000000000).simulate())
