6ed6c1c32b57d1b06fea60cea8291da119847278
Joy Gu <joy.alice.gu@gmail.com>
2023-12-10 04:57:34 -0800

    2023 day 8 part 2

--- 2023/src/day08.rs
+++ 2023/src/day08.rs
@@ -1,5 +1,6 @@
 use crate::Solution;
 use itertools::Itertools;
+use num::integer::lcm;
 use regex::Regex;
 use std::collections::HashMap;
 
@@ -13,27 +14,31 @@ pub enum Direction {
 type Node = String;
 
 impl Day08 {
-    fn steps(
+    fn traverse(
         directions: &Vec<Direction>,
         network: &HashMap<Node, (Node, Node)>,
+        direction_index: usize,
         src: Node,
-        dst: Node,
-    ) -> usize {
+        at_dst: &dyn Fn(&Node) -> bool,
+    ) -> (Node, usize) {
         let mut node = &src;
         let mut steps: usize = 0;
-        let mut i = 0;
-        while node != &dst {
+        let mut i = direction_index;
+        loop {
             node = match directions[i] {
-                Direction::Left => &network[node].0,
-                Direction::Right => &network[node].1,
+                Direction::Left => &network.get(node).unwrap().0,
+                Direction::Right => &network.get(node).unwrap().1,
             };
             i = (i + 1) % directions.len();
             steps += 1;
+            if at_dst(node) {
+                break;
+            }
             if node == &src && i == 0 {
                 panic!("Day 8: Can't find a path between nodes")
             }
         }
-        steps
+        (node.to_string(), steps)
     }
 }
 
@@ -68,11 +73,41 @@ impl Solution for Day08 {
     fn part_one(_parsed_input: &mut Self::ParsedInput) -> String {
         let (directions, network) = _parsed_input;
         let (src, dst) = ("AAA".to_string(), "ZZZ".to_string());
-        Self::steps(&directions, &network, src, dst).to_string()
+        let at_dst = |node: &Node| node == &dst;
+        Self::traverse(&directions, &network, 0, src, &at_dst)
+            .1
+            .to_string()
     }
 
     fn part_two(_parsed_input: &mut Self::ParsedInput) -> String {
-        "0".to_string()
+        let (directions, network) = _parsed_input;
+        let at_dst = |node: &Node| node.ends_with('Z');
+        let srcs = network
+            .keys()
+            .filter(|n| n.ends_with('A'))
+            .map(|n| n.to_string());
+        let (dsts, paths): (Vec<Node>, Vec<usize>) = srcs
+            .map(|src| Self::traverse(&directions, &network, 0, src, &at_dst))
+            .unzip();
+        {
+            // assert that your solution works for the given input
+            let cycles: Vec<usize> = dsts
+                .into_iter()
+                .enumerate()
+                .map(|(j, dst)| {
+                    let i = paths[j] % directions.len();
+                    let at_dst = |node: &Node| node == &dst;
+                    Self::traverse(&directions, &network, i, dst.clone(), &at_dst).1
+                })
+                .collect();
+            assert!(
+                paths.iter().zip(cycles.iter()).all(|(p, c)| p == c),
+                "paths {:?} != cycles {:?}",
+                paths,
+                cycles
+            );
+        }
+        paths.into_iter().fold(1, |acc, n| lcm(acc, n)).to_string()
     }
 }
 
@@ -107,6 +142,25 @@ ZZZ = (ZZZ, ZZZ)"
 AAA = (BBB, BBB)
 BBB = (AAA, ZZZ)
 ZZZ = (ZZZ, ZZZ)"
+            ),
+            "6".to_string()
+        )
+    }
+
+    #[test]
+    fn check_day08_part2_case1() {
+        assert_eq!(
+            Day08::solve_part_two(
+                "LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)"
             ),
             "6".to_string()
         )
