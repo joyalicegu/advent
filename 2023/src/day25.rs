50f997923afecfae4a5bbdc5064d718d7c9b699d
Joy Gu <joy.alice.gu@gmail.com>
2024-01-28 17:49:15 -0800

    2023 day 25 (this is someone else's solution, not mine) (sorry _)

--- 2023/src/day25.rs
+++ 2023/src/day25.rs
@@ -1,78 +1,55 @@
 use crate::Solution;
-use itertools::Itertools;
-use petgraph::algo::has_path_connecting;
-use petgraph::prelude::*;
-use petgraph::Graph;
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 
 pub struct Day25;
 
 impl Day25 {
-    fn find_component_sizes(graph: &UnGraph<(), ()>) -> (usize, usize) {
-        let edge_cut: Vec<(EdgeIndex, EdgeIndex, EdgeIndex)> = graph
-            .edge_indices()
-            .combinations(3)
-            .map(|combo| {
-                let (a, b, c) = combo.iter().collect_tuple().unwrap();
-                (*a, *b, *c)
-            })
-            .collect();
-        for (i, j, k) in edge_cut {
-            let (iu, iv) = graph.edge_endpoints(i).unwrap();
-            let (ju, jv) = graph.edge_endpoints(j).unwrap();
-            let (ku, kv) = graph.edge_endpoints(k).unwrap();
-            let mut cut_graph = graph.clone();
-            cut_graph.remove_edge(cut_graph.find_edge(iu, iv).unwrap());
-            cut_graph.remove_edge(cut_graph.find_edge(ju, jv).unwrap());
-            cut_graph.remove_edge(cut_graph.find_edge(ku, kv).unwrap());
-            if has_path_connecting(&cut_graph, iu, iv, None) {
-                // not an edge cut
-                continue;
-            } else {
-                println!("edge cut: {:?}", (i, j, k));
-                let mut count = 0;
-                let mut count2 = 0;
-                for node in cut_graph.node_indices() {
-                    if has_path_connecting(&cut_graph, iu, node, None) {
-                        count += 1;
-                    } else {
-                        count2 += 1;
-                    }
-                }
-                println!("component sizes: {:?}", (count, count2));
-                return (count, count2);
-            }
+    fn count(
+        v: &String,
+        graph: &HashMap<String, HashSet<String>>,
+        vertices: &HashSet<String>,
+    ) -> usize {
+        let total = graph.get(v).unwrap();
+        total.difference(vertices).count()
+    }
+
+    fn find_component_sizes(graph: &HashMap<String, HashSet<String>>) -> (usize, usize) {
+        // from https://www.reddit.com/r/adventofcode/comments/18qbsxs/comment/ketzp94/
+        let mut vertices = graph.keys().map(|s| s.clone()).collect::<HashSet<String>>();
+        while vertices
+            .iter()
+            .map(|v| Self::count(v, graph, &vertices))
+            .sum::<usize>()
+            != 3
+        {
+            let (_, v) = vertices
+                .iter()
+                .map(|v| (Self::count(v, graph, &vertices), v))
+                .max()
+                .unwrap();
+            vertices.remove(&(v.clone()));
         }
-        panic!("failed to find edge cut");
+        (vertices.len(), graph.keys().count() - vertices.len())
     }
 }
 
 impl Solution for Day25 {
-    type ParsedInput = UnGraph<(), ()>;
+    type ParsedInput = HashMap<String, HashSet<String>>;
 
     fn parse_input(input_lines: &str) -> Self::ParsedInput {
-        let mut graph = Graph::new_undirected();
-        let mut nodes = HashMap::new();
+        let mut adj = HashMap::new();
         for line in input_lines.lines() {
-            let (u_str, etc) = line.split_once(": ").unwrap();
-            for v_str in etc.split_whitespace() {
-                let u_key = u_str.to_string();
-                if !nodes.contains_key(&u_key) {
-                    nodes.insert(u_key.clone(), graph.add_node(()));
-                }
-                let u = *nodes.get(&u_key).unwrap();
-
-                let v_key = v_str.to_string();
-                if !nodes.contains_key(&v_key) {
-                    nodes.insert(v_key.clone(), graph.add_node(()));
-                }
-                let v = *nodes.get(&v_key).unwrap();
-
-                graph.add_edge(u, v, ());
+            let (u, etc) = line.split_once(": ").unwrap();
+            for v in etc.split_whitespace() {
+                adj.entry(u.to_string())
+                    .or_insert(HashSet::new())
+                    .insert(v.to_string());
+                adj.entry(v.to_string())
+                    .or_insert(HashSet::new())
+                    .insert(u.to_string());
             }
         }
-        println!("graph: {:?}", graph);
-        graph
+        adj
     }
 
     fn part_one(_parsed_input: &mut Self::ParsedInput) -> String {
