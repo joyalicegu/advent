1769e4b6eb28846ba2bcf97375d085a53d2a4910
Joy Gu <joy.alice.gu@gmail.com>
2025-10-30 20:41:08 -0700

    2024 day 6 part 1

--- 2024/src/day06.rs
+++ 2024/src/day06.rs
@@ -1,23 +1,70 @@
 use crate::Solution;
+use std::collections::HashSet;
 
 pub struct Day06;
 
 impl Day06 {
-    fn find_start(pipes: &Vec<Vec<char>>) -> (usize, usize) {
-        pipes
-            .iter()
+    fn find_start(grid: &Vec<Vec<char>>) -> (usize, usize) {
+        grid.iter()
             .enumerate()
             .filter_map(|(r, row)| {
                 row.iter()
                     .enumerate()
-                    .filter_map(|(c, &pipe)| if pipe == 'S' { Some((r, c)) } else { None })
+                    .filter_map(|(c, &ch)| {
+                        if ch == '^' || ch == '>' || ch == '<' || ch == 'v' {
+                            Some((r, c))
+                        } else {
+                            None
+                        }
+                    })
                     .next()
             })
             .next()
             .unwrap()
     }
 
-    // TODO helpers
+    fn turn_right(dr: isize, dc: isize) -> (isize, isize) {
+        match (dr, dc) {
+            (0, 1) => (1, 0),
+            (1, 0) => (0, -1),
+            (0, -1) => (-1, 0),
+            (-1, 0) => (0, 1),
+            _ => panic!("Invalid character"),
+        }
+    }
+
+    fn patrol(grid: &Vec<Vec<char>>) -> Vec<(usize, usize)> {
+        let mut positions = Vec::new();
+        let (mut r, mut c) = Self::find_start(grid);
+        let (mut dr, mut dc): (isize, isize) = match grid[r][c] {
+            '>' => (0, 1),
+            'v' => (1, 0),
+            '<' => (0, -1),
+            '^' => (-1, 0),
+            _ => panic!("Invalid character"),
+        };
+        let max_r = grid.len();
+        let max_c = grid[0].len();
+        loop {
+            positions.push((r, c)); // add current position to path
+            if (r == 0 && dr < 0)
+                || (c == 0 && dc < 0)
+                || (r == max_r - 1 && dr > 0)
+                || (c == max_c - 1 && dc > 0)
+            {
+                break; // leaving the grid
+            }
+            let (nr, nc) = ((r as isize + dr) as usize, (c as isize + dc) as usize);
+            if grid[nr][nc] == '#' {
+                // encountered an obstacle
+                (dr, dc) = Self::turn_right(dr, dc);
+                (r, c) = ((r as isize + dr) as usize, (c as isize + dc) as usize);
+            } else {
+                (r, c) = (nr, nc);
+            }
+        }
+        positions
+    }
 }
 
 impl Solution for Day06 {
@@ -32,9 +79,9 @@ impl Solution for Day06 {
 
     fn part_one(_parsed_input: &mut Self::ParsedInput) -> String {
         let grid = _parsed_input;
-        Self::count_guard_positions(
-        "0".to_string()
-        // TODO
+        HashSet::<(usize, usize)>::from_iter(Self::patrol(grid))
+            .len()
+            .to_string()
     }
 
     fn part_two(_parsed_input: &mut Self::ParsedInput) -> String {
