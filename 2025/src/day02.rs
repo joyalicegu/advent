dfbd1fe991b07522c720f59a8502cd20e13bfa65
Joy Gu <joy.alice.gu@gmail.com>
2025-12-11 20:52:46 -0800

    2025 day 2 part 2

--- 2025/src/day02.rs
+++ 2025/src/day02.rs
@@ -1,24 +1,26 @@
 use crate::Solution;
 use itertools::Itertools;
+use std::collections::HashSet;
 
 pub struct Day02;
 
 impl Day02 {
-    fn sum_invalid_ids(ranges: &Vec<(u32, u32)>) -> u32 {
-        let mut result = 0;
+    fn sum_invalid_ids(ranges: &Vec<(u64, u64)>, part_two: bool) -> u64 {
+        let mut invalid_ids = HashSet::new();
         for (lo, hi) in ranges.iter() {
             // do this the dumb way first
             // generate them, and count them one at a time
-            println!("\nlo {:?} hi {:?}", lo, hi);
-            let digits_low = lo.to_string().len();
-            let digits_high = hi.to_string().len();
-            for digits in digits_low..=digits_high {
-                for repeats in 2..=digits {
+            // println!("\nlo {:?} hi {:?}", lo, hi);
+            let digits_lo = lo.to_string().len();
+            let digits_hi = hi.to_string().len();
+            for digits in digits_lo..=digits_hi {
+                let max_repeats = if part_two { digits } else { 2 };
+                for repeats in 2..=max_repeats {
                     if digits % repeats != 0 {
                         continue;
                     }
-                    println!("digits {:?} repeats {:?}", digits, repeats);
-                    let multiplier = 10_u32.pow((digits / repeats) as u32);
+                    // println!("digits {:?} repeats {:?}", digits, repeats);
+                    let multiplier = 10_u64.pow((digits / repeats) as u32);
                     let mut n = multiplier / 10;
                     let mut nn = (0..repeats).map(|p| n * multiplier.pow(p as u32)).sum();
                     while nn < *lo {
@@ -30,7 +32,7 @@ impl Day02 {
                     }
                     while *lo <= nn && nn <= *hi {
                         // println!("{:?}", nn);
-                        result += nn;
+                        invalid_ids.insert(nn);
                         n += 1;
                         if n > multiplier - 1 {
                             break;
@@ -40,12 +42,12 @@ impl Day02 {
                 }
             }
         }
-        result
+        invalid_ids.iter().sum()
     }
 }
 
 impl Solution for Day02 {
-    type ParsedInput = Vec<(u32, u32)>;
+    type ParsedInput = Vec<(u64, u64)>;
 
     fn parse_input(input_lines: &str) -> Self::ParsedInput {
         input_lines
@@ -55,7 +57,7 @@ impl Solution for Day02 {
             .split(",")
             .map(|l| {
                 l.split("-")
-                    .map(|n| n.parse::<u32>().unwrap())
+                    .map(|n| n.parse::<u64>().unwrap())
                     .collect_tuple()
                     .unwrap()
             })
@@ -63,12 +65,11 @@ impl Solution for Day02 {
     }
 
     fn part_one(_parsed_input: &mut Self::ParsedInput) -> String {
-        Self::sum_invalid_ids(_parsed_input).to_string()
+        Self::sum_invalid_ids(_parsed_input, false).to_string()
     }
 
     fn part_two(_parsed_input: &mut Self::ParsedInput) -> String {
-        "0".to_string()
-        // TODO
+        Self::sum_invalid_ids(_parsed_input, true).to_string()
     }
 }
 
@@ -81,5 +82,10 @@ mod tests {
     #[test]
     fn check_day02_part1_case1() {
         assert_eq!(Day02::solve_part_one(TEST_INPUT), "1227775554".to_string())
+    }
+
+    #[test]
+    fn check_day02_part2_case1() {
+        assert_eq!(Day02::solve_part_two(TEST_INPUT), "4174379265".to_string())
     }
 }
