49655be10e7fce5e13d63d5d485fe1d9bd2a4f98
Joy Gu <joy.alice.gu@gmail.com>
2025-12-22 13:02:12 -0800

    2025 day 4 part 2 wip

--- 2025/src/day04.rs
+++ 2025/src/day04.rs
@@ -1,5 +1,6 @@
 use crate::Solution;
 use itertools::iproduct;
+use std::collections::{HashMap, HashSet};
 
 pub struct Day04;
 
@@ -10,47 +11,63 @@ impl Day04 {
             .collect()
     }
 
-    fn accessible(grid: &Vec<Vec<char>>) -> usize {
-        let rows = grid.len();
-        let cols = grid[0].len();
-        let mut count = 0;
+    fn graph(grid: &Vec<Vec<char>>) -> HashMap<(usize, usize), HashSet<(usize, usize)>> {
+        let (rows, cols) = (grid.len(), grid[0].len());
+        let mut adj = HashMap::new();
         for r in 0..rows {
             for c in 0..cols {
                 if grid[r][c] != '@' {
                     continue;
                 }
-                let mut adjacent = 0;
                 for &(nr, nc) in Self::neighbors(r, c, rows, cols).iter() {
                     if grid[nr][nc] == '@' {
-                        adjacent += 1
-                    };
+                        adj.entry((r, c)).or_insert(HashSet::new()).insert((nr, nc));
+                    }
                 }
-                if adjacent < 4 {
-                    count += 1;
-                }
+            }
+        }
+        adj
+    }
+
+    fn accessible(adj: &HashMap<(usize, usize), HashSet<(usize, usize)>>, k: usize) -> usize {
+        let mut count = 0;
+        for (node, neighbors) in adj {
+            println!("node: {:?}, neighbors: {:?}", node, neighbors);
+            if neighbors.len() < k {
+                count += 1;
             }
         }
         count
     }
+
+    fn k_core(
+        adj: &HashMap<(usize, usize), HashSet<(usize, usize)>>,
+        k: usize,
+    ) -> HashMap<(usize, usize), HashSet<(usize, usize)>> {
+        // https://en.wikipedia.org/wiki/Degeneracy_(graph_theory)#k-Cores
+        HashMap::new() // TODO
+    }
 }
 
 impl Solution for Day04 {
-    type ParsedInput = Vec<Vec<char>>;
+    type ParsedInput = HashMap<(usize, usize), HashSet<(usize, usize)>>;
 
     fn parse_input(input_lines: &str) -> Self::ParsedInput {
-        input_lines
+        let grid: Vec<Vec<char>> = input_lines
             .lines()
             .map(|line| line.chars().collect())
-            .collect()
+            .collect();
+        Self::graph(&grid)
     }
 
     fn part_one(_parsed_input: &mut Self::ParsedInput) -> String {
-        Self::accessible(&_parsed_input).to_string()
+        let adj = _parsed_input;
+        Self::accessible(adj, 4).to_string()
     }
 
     fn part_two(_parsed_input: &mut Self::ParsedInput) -> String {
-        "0".to_string()
-        // TODO
+        let adj = _parsed_input;
+        Self::k_core(adj, 4).keys().len().to_string()
     }
 }
 
@@ -72,5 +89,10 @@ mod tests {
     #[test]
     fn check_day04_part1_case1() {
         assert_eq!(Day04::solve_part_one(TEST_INPUT), "13".to_string())
+    }
+
+    #[test]
+    fn check_day04_part2_case1() {
+        assert_eq!(Day04::solve_part_one(TEST_INPUT), "43".to_string())
     }
 }
