f98b222d3942e1cb50e395a4e6bce089587a6709
Joy Gu <joy.alice.gu@gmail.com>
2025-12-23 00:49:02 -0800

    2025 day 6 part 2

--- 2025/src/day06.rs
+++ 2025/src/day06.rs
@@ -4,13 +4,23 @@ use std::iter::zip;
 pub struct Day06;
 
 impl Day06 {
-    // TODO helpers
-}
+    fn calculate(problems: &Vec<(String, Vec<u64>)>) -> u64 {
+        problems
+            .iter()
+            .map(|(op, operands)| {
+                if op == "*" {
+                    operands.iter().product::<u64>()
+                } else if op == "+" {
+                    operands.iter().sum::<u64>()
+                } else {
+                    0
+                }
+            })
+            .sum::<u64>()
+    }
 
-impl Solution for Day06 {
-    type ParsedInput = Vec<(String, Vec<u64>)>;
-
-    fn parse_input(input_lines: &str) -> Self::ParsedInput {
+    fn parse_input_one(input_lines: &str) -> Vec<(String, Vec<u64>)> {
+        // not very nice
         let tmp: Vec<Vec<u64>> = input_lines
             .lines()
             .map(|line| {
@@ -34,25 +44,56 @@ impl Solution for Day06 {
         zip(ops.into_iter(), operands.into_iter()).collect()
     }
 
+    fn parse_input_two(input_lines: &str) -> Vec<(String, Vec<u64>)> {
+        // not very nice
+        let chars: Vec<Vec<char>> = input_lines
+            .lines()
+            .map(|line| line.chars().collect())
+            .collect();
+        let (rows, cols) = (chars.len() - 1, chars[0].len());
+        let transposed: Vec<String> = (0..cols)
+            .map(|c| (0..rows).map(|r| chars[r][c]).collect())
+            .collect();
+        let mut operands = Vec::new();
+        let mut problem = Vec::new();
+        for thing in transposed.iter() {
+            let trimmed = thing.trim();
+            if let Ok(operand) = trimmed.parse::<u64>() {
+                problem.push(operand);
+            } else {
+                operands.push(problem);
+                problem = Vec::new();
+            }
+        }
+        operands.push(problem);
+        let ops: Vec<String> = input_lines
+            .lines()
+            .rev()
+            .next()
+            .unwrap()
+            .split_whitespace()
+            .map(|s| s.to_string())
+            .collect();
+        zip(ops.into_iter(), operands.into_iter()).collect()
+    }
+}
+
+impl Solution for Day06 {
+    type ParsedInput = (Vec<(String, Vec<u64>)>, Vec<(String, Vec<u64>)>);
+
+    fn parse_input(input_lines: &str) -> Self::ParsedInput {
+        (
+            Self::parse_input_one(input_lines),
+            Self::parse_input_two(input_lines),
+        )
+    }
+
     fn part_one(_parsed_input: &mut Self::ParsedInput) -> String {
-        _parsed_input
-            .iter()
-            .map(|(op, operands)| {
-                if op == "*" {
-                    operands.iter().product::<u64>()
-                } else if op == "+" {
-                    operands.iter().sum::<u64>()
-                } else {
-                    0
-                }
-            })
-            .sum::<u64>()
-            .to_string()
+        Self::calculate(&_parsed_input.0).to_string()
     }
 
     fn part_two(_parsed_input: &mut Self::ParsedInput) -> String {
-        "0".to_string()
-        // TODO
+        Self::calculate(&_parsed_input.1).to_string()
     }
 }
 
@@ -60,13 +101,18 @@ impl Solution for Day06 {
 mod tests {
     use super::*;
 
-    const TEST_INPUT: &str = "123 328  51 64
- 45 64  387 23
+    const TEST_INPUT: &str = "123 328  51 64 
+ 45 64  387 23 
   6 98  215 314
 *   +   *   +  ";
 
     #[test]
     fn check_day06_part1_case1() {
         assert_eq!(Day06::solve_part_one(TEST_INPUT), "4277556".to_string())
+    }
+
+    #[test]
+    fn check_day06_part2_case1() {
+        assert_eq!(Day06::solve_part_two(TEST_INPUT), "3263827".to_string())
     }
 }
