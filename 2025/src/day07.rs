47ade07e77e5b72fa203843aef1b2eeaeb57d899
Joy Gu <joy.alice.gu@gmail.com>
2025-12-24 14:12:51 -0800

    2025 day 7 part 2

--- 2025/src/day07.rs
+++ 2025/src/day07.rs
@@ -1,5 +1,5 @@
 use crate::Solution;
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 pub struct Day07;
 
@@ -29,6 +29,44 @@ impl Day07 {
         }
         total
     }
+
+    fn count_timelines(diagram: &Vec<Vec<char>>) -> usize {
+        let start = diagram[0].iter().position(|&ch| ch == 'S').unwrap();
+        let width = diagram[0].len();
+        let mut beams: HashMap<usize, usize> = HashMap::from([(start, 1)]);
+        for row in 1..diagram.len() {
+            let splitters: HashSet<usize> = diagram[row]
+                .iter()
+                .enumerate()
+                .filter(|&(_, &ch)| ch == '^')
+                .map(|(i, _)| i)
+                .collect();
+            let mut next_beams: HashMap<usize, usize> = HashMap::new();
+            for (beam, weight) in beams.iter() {
+                if splitters.contains(&beam) {
+                    if *beam >= 1 {
+                        next_beams
+                            .entry(beam - 1)
+                            .and_modify(|w| *w += *weight)
+                            .or_insert(*weight);
+                    }
+                    if *beam < width - 1 {
+                        next_beams
+                            .entry(beam + 1)
+                            .and_modify(|w| *w += *weight)
+                            .or_insert(*weight);
+                    }
+                } else {
+                    next_beams
+                        .entry(*beam)
+                        .and_modify(|w| *w += *weight)
+                        .or_insert(*weight);
+                }
+            }
+            beams = next_beams;
+        }
+        beams.iter().map(|(_, w)| w).sum::<usize>()
+    }
 }
 
 impl Solution for Day07 {
@@ -47,8 +85,8 @@ impl Solution for Day07 {
     }
 
     fn part_two(_parsed_input: &mut Self::ParsedInput) -> String {
-        "0".to_string()
-        // TODO
+        let diagram = _parsed_input;
+        Self::count_timelines(diagram).to_string()
     }
 }
 
@@ -76,5 +114,10 @@ mod tests {
     #[test]
     fn check_day07_part1_case1() {
         assert_eq!(Day07::solve_part_one(TEST_INPUT), "21".to_string())
+    }
+
+    #[test]
+    fn check_day07_part2_case1() {
+        assert_eq!(Day07::solve_part_two(TEST_INPUT), "40".to_string())
     }
 }
