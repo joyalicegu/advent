355f874301c47f6ef2b68a29a14dca735f234d7e
Joy Gu <joy.alice.gu@gmail.com>
2026-05-02 17:25:02 -0700

    2025 day 9 part 2 wip

--- 2025/src/day09.rs
+++ 2025/src/day09.rs
@@ -15,6 +15,7 @@ impl Day09 {
     }
 
     fn edges(polygon: &Vec<(isize, isize)>) -> Vec<((isize, isize), (isize, isize))> {
+        // cribbed from area() from my 2023 day 18
         polygon
             .clone()
             .into_iter()
@@ -24,8 +25,43 @@ impl Day09 {
             .collect()
     }
 
+    fn is_inside(
+        a: (isize, isize),
+        b: (isize, isize),
+        boundary_edges: &Vec<((isize, isize), (isize, isize))>,
+    ) -> bool {
+        let (x_min, x_max) = (cmp::min(a.0, b.0), cmp::max(a.0, b.0));
+        // bottom top
+        let (y_min, y_max) = (cmp::min(a.1, b.1), cmp::max(a.1, b.1));
+        for e in boundary_edges.iter() {
+            let (e_x_min, e_x_max) = (cmp::min(e.0 .0, e.1 .0), cmp::max(e.0 .0, e.1 .0));
+            let (e_y_min, e_y_max) = (cmp::min(e.0 .1, e.1 .1), cmp::max(e.0 .1, e.1 .1));
+            if e_x_min == e_x_max {
+                // boundary edge is vertical
+                let e_x = e_x_min;
+                if x_min < e_x && e_x < x_max && e_y_max > y_min && e_y_min < y_max {
+                    return false;
+                }
+            } else if e_y_min == e_y_max {
+                // boundary edge is horizontal
+                let e_y = e_y_min;
+                if y_min < e_y && e_y < y_max && e_x_max > x_min && e_x_min < x_max {
+                    return false;
+                }
+            }
+        }
+        true
+    }
+
     fn largest_area_in_polygon(corners: &Vec<(isize, isize)>) -> isize {
-        0 // TODO
+        let boundary_edges = Self::edges(corners);
+        corners
+            .iter()
+            .tuple_combinations()
+            .filter(|(a, b)| Self::is_inside(**a, **b, &boundary_edges))
+            .map(|(a, b)| ((a.0 - b.0 + 1) * (a.1 - b.1 + 1)).abs())
+            .max()
+            .expect("corners.len() should be at least 2")
     }
 }
 
