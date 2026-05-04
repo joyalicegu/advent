fa975bbe2ebe83c550d69107353f663edd7a426d
Joy Gu <joy.alice.gu@gmail.com>
2026-05-03 17:56:24 -0700

    2025 day 9 part 2 wip

--- 2025/src/day09.rs
+++ 2025/src/day09.rs
@@ -1,84 +1,117 @@
 use crate::Solution;
+use itertools::sorted;
 use itertools::Itertools;
-use std::cmp;
+use std::collections::{HashMap, HashSet};
 
 pub struct Day09;
 
 impl Day09 {
-    fn largest_area(corners: &Vec<(isize, isize)>) -> isize {
+    fn largest_area(corners: &Vec<(usize, usize)>) -> usize {
         corners
             .iter()
             .tuple_combinations()
-            .map(|(a, b)| ((a.0 - b.0 + 1) * (a.1 - b.1 + 1)).abs())
+            .map(|(ua, ub)| {
+                let a = (ua.0 as isize, ua.1 as isize);
+                let b = (ub.0 as isize, ub.1 as isize);
+                ((a.0 - b.0 + 1) * (a.1 - b.1 + 1)).abs()
+            })
             .max()
-            .expect("corners.len() should be at least 2")
-    }
-
-    // TODO nvm
-    // TODO just do coordinate compression
-    // TODO and flood fill to color green tiles
-    // TODO and then just brute force check all tiles in each rectangle
-
-    fn edges(polygon: &Vec<(isize, isize)>) -> Vec<((isize, isize), (isize, isize))> {
-        // cribbed from area() from my 2023 day 18
-        polygon
-            .clone()
-            .into_iter()
-            .cycle()
-            .tuple_windows()
-            .take(polygon.len())
-            .collect()
+            .expect("corners.len() should be at least 2") as usize
     }
 
     fn is_inside(
-        a: (isize, isize),
-        b: (isize, isize),
-        boundary_edges: &Vec<((isize, isize), (isize, isize))>,
+        ca: (usize, usize),
+        cb: (usize, usize),
+        compressed_tiles: &Vec<Vec<char>>,
     ) -> bool {
-        let (x_min, x_max) = (cmp::min(a.0, b.0), cmp::max(a.0, b.0));
-        // bottom top
-        let (y_min, y_max) = (cmp::min(a.1, b.1), cmp::max(a.1, b.1));
-        for e in boundary_edges.iter() {
-            let (e_x_min, e_x_max) = (cmp::min(e.0 .0, e.1 .0), cmp::max(e.0 .0, e.1 .0));
-            let (e_y_min, e_y_max) = (cmp::min(e.0 .1, e.1 .1), cmp::max(e.0 .1, e.1 .1));
-            if e_x_min == e_x_max {
-                // boundary edge is vertical
-                let e_x = e_x_min;
-                if x_min < e_x && e_x < x_max && e_y_max > y_min && e_y_min < y_max {
-                    return false;
+        false
+        // TODO is inside: and then just brute force check all tiles in each rectangle (compressed)
+    }
+
+    fn largest_area_in_polygon(corners: &Vec<(usize, usize)>) -> isize {
+        let xs: HashSet<usize> = corners.iter().map(|(x, _)| *x).collect();
+        let ys: HashSet<usize> = corners.iter().map(|(_, y)| *y).collect();
+        let col_to_x: Vec<usize> = sorted(xs.into_iter()).collect();
+        let row_to_y: Vec<usize> = sorted(ys.into_iter()).collect();
+        let x_to_col: HashMap<usize, usize> = col_to_x
+            .iter()
+            .enumerate()
+            .map(|(i, c)| (*c, i as usize))
+            .collect();
+        let y_to_row: HashMap<usize, usize> = row_to_y
+            .iter()
+            .enumerate()
+            .map(|(i, c)| (*c, i as usize))
+            .collect();
+        let compressed_corners: Vec<(usize, usize)> = corners
+            .iter()
+            .map(|(x, y)| (*y_to_row.get(y).unwrap(), *x_to_col.get(x).unwrap()))
+            .collect();
+        let (cols, rows) = (x_to_col.len(), y_to_row.len());
+        let mut compressed_tiles = vec![vec![' '; cols]; rows];
+        // color tiles: corners and edges
+        let segments: Vec<_> = compressed_corners
+            .clone()
+            .into_iter()
+            .skip(1)
+            .zip(compressed_corners.clone().into_iter())
+            .collect();
+        for &((r, c), (nr, nc)) in segments.iter() {
+            compressed_tiles[r][c] = '#';
+            compressed_tiles[nr][nc] = '#';
+            if r < nr {
+                for ir in (r + 1)..nr {
+                    compressed_tiles[ir][c] = 'X';
                 }
-            } else if e_y_min == e_y_max {
-                // boundary edge is horizontal
-                let e_y = e_y_min;
-                if y_min < e_y && e_y < y_max && e_x_max > x_min && e_x_min < x_max {
-                    return false;
+            } else if nr < r {
+                for ir in (nr + 1)..r {
+                    compressed_tiles[ir][c] = 'X';
+                }
+            }
+            if c < nc {
+                for ic in (c + 1)..nc {
+                    compressed_tiles[r][ic] = 'X';
+                }
+            } else if nc < c {
+                for ic in (nc + 1)..c {
+                    compressed_tiles[r][ic] = 'X';
                 }
             }
         }
-        true
-    }
-
-    fn largest_area_in_polygon(corners: &Vec<(isize, isize)>) -> isize {
-        let boundary_edges = Self::edges(corners);
+        // color tiles: flood fill with Xs
+        // TODO actually flood fill
+        // debug: print the outline
+        for ir in 0..rows {
+            for ic in 0..cols {
+                print!("{}", compressed_tiles[ir][ic]);
+            }
+            println!("");
+        }
+        // TODO implement the inside check
+        // TODO map to real coordinates to get area
         corners
             .iter()
             .tuple_combinations()
-            .filter(|(a, b)| Self::is_inside(**a, **b, &boundary_edges))
-            .map(|(a, b)| ((a.0 - b.0 + 1) * (a.1 - b.1 + 1)).abs())
+            .filter(|(a, b)| Self::is_inside(**a, **b, &compressed_tiles))
+            .map(|(ua, ub)| {
+                let a = (ua.0 as isize, ua.1 as isize);
+                let b = (ub.0 as isize, ub.1 as isize);
+                ((a.0 - b.0 + 1) * (a.1 - b.1 + 1)).abs()
+            })
             .max()
             .expect("corners.len() should be at least 2")
     }
 }
 
 impl Solution for Day09 {
-    type ParsedInput = Vec<(isize, isize)>;
+    type ParsedInput = Vec<(usize, usize)>;
 
     fn parse_input(input_lines: &str) -> Self::ParsedInput {
         input_lines
             .lines()
             .map(|line| {
                 line.split(",")
-                    .map(|s| s.parse::<isize>().unwrap())
+                    .map(|s| s.parse::<usize>().unwrap())
                     .collect_tuple()
                     .unwrap()
             })
